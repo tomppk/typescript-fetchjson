9f3b15e6bfdabf311d6f7cf881890fbd2b2ddd5d
tomppk <tomi.kivioja@gmail.com>
2021-03-16 16:41:31 +0200

    testing typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
 import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// Interfaces define the structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The ToDo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished? ${completed}
+`);
+};
